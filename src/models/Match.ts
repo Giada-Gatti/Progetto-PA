33c17318f13773e07d29f344ee926f80b43468ab
Giada <s1108648@studenti.univpm.it>
2024-06-15 13:35:25 +0200

    Create models

---
+++ src/models/Match.ts
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database';
+
+
+export class Match extends Model {
+  public id!: number;
+  public type!: string;
+  public status!: string;
+  public currentPlayerId!: number;
+//   public opponentEmail?: string;
+  public opponentPlayerId!: number;
+  public winnerId?: number;
+  public maxTime?: number;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  currentPlayerId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  opponentPlayerId: {
+    type: DataTypes.INTEGER,
+  },
+  winnerId: {
+    type: DataTypes.INTEGER,
+  },
+  maxTime: {
+    type: DataTypes.INTEGER,
+  }
+}, {
+  sequelize,
+  modelName: 'Match',
+});
---
+++ src/models/Move.ts
@@ -0,0 +1,44 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database';
+
+export enum Symbol {
+    X = 'X',
+    O = 'O'
+}
+
+export class Move extends Model {
+  public id!: number;
+  public playerId!: number;
+  public matchId!: number;
+  public symbol!: Symbol;
+  public position!: number;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+Move.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  playerId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  matchId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  position: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  symbol : {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'Move',
+});
